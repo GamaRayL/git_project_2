4a78e1e200a7ee57744af2de49e292f811390d26
Gamid <gamaizingg@gmail.com>
2023-06-05 00:23:01 +0300

    _sum

---
+++ main.py
@@ -0,0 +1,12 @@
+from funcs import _sum
+
+
+def print_hi(name):
+    print(_sum())
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
